b08928fe8d3b90a7b2a039e951266676ea62d25b
Jeeva-G <jeeva.analyst@gmail.com>
2016-02-21 14:40:36 +1300

    Updated the import data file.

--- HotelsReco/Dataload/Importdata.py
+++ HotelsReco/Dataload/Importdata.py
@@ -1,15 +1,37 @@
 # Reading hotel details from offering file & user reviews from reviews file.
 """This script reads the json files and make a python list out of it."""
-import json
+# import json
+import ujson
+import pandas as pd
 
-hoteldata = []
-with open("/Users/jeevananthamganesan/Hotel-recommendation-system/\
+hotellist = []
+with open("/Users/jeevananthamganesan/Documents/\
 Hotel_review_dataset/offering.json") as f:
     for line in f:
-        hoteldata.append(json.loads(line))
+        h = ujson.loads(line)
+        hotellist.append(
+                         {"offering_id": h["id"],
+                             "Hotel": h["name"]
+                          }
+        )
 
-reviewdata = []
-with open("/Users/jeevananthamganesan/Hotel-recommendation-system/\
-Hotel_review_dataset/review.json") as r:
-    for line in r:
-        reviewdata.append(json.loads(line))
+hoteldata = pd.DataFrame(hotellist)
+
+reviewlist = []
+with open("/Users/jeevananthamganesan/Documents/\
+Hotel_review_dataset/review.json") as f:
+    for line in f:
+        d = ujson.loads(line)
+        reviewlist.append(
+            {"offering_id": d["offering_id"],
+             "username": d["author"]["username"],
+             "pub_date": d["date"],
+             "comment": d["text"],
+             "comment": d["ratings"]["overall"]
+             }
+        )
+
+reviewdata = pd.DataFrame(reviewlist)
+
+pd.set_option('display.expand_frame_repr', False)
+data = pd.merge(hoteldata, reviewdata, how='inner')
