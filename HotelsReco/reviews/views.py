3ea726790bcba0eb22a9733b2a0e2cbc08eb5a3b
Jeeva-G <jeeva.analyst@gmail.com>
2016-02-23 15:57:01 +1300

    Updated the view file for HTML page.

--- HotelsReco/reviews/views.py
+++ HotelsReco/reviews/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+"""URLs with reviews and empty suffix will be pointed this page."""
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Review, Hotel
+
+
+def review_list(request):
+    """Use this function to display the list of reviews."""
+    latest_review_list = Review.objects.order_by('-pub_date')[:9]
+    context = {'latest_review_list': latest_review_list}
+    return render(request, 'reviews/review_list.html', context)
+
+
+def review_detail(request, review_id):
+    """Use this function to display the detail for each review."""
+    review = get_object_or_404(Review, pk=review_id)
+    return render(request, 'reviews/review_detail.html', {'review': review})
+
+
+def Hotel_list(request):
+    """Use this function to display the hotel list."""
+    Hotel_list = Hotel.objects.order_by('-name')
+    context = {'wine_list': Hotel_list}
+    return render(request, 'reviews/Hotel_list.html', context)
