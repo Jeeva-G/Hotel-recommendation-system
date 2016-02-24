dae8ff08a4cd0c3ba14f0f50aabb0daf51d182a0
Jeeva-G <jeeva.analyst@gmail.com>
2016-02-24 14:40:28 +1300

    Updated the hotel detail function.

--- HotelsReco/reviews/views.py
+++ HotelsReco/reviews/views.py
@@ -19,6 +19,12 @@ def review_detail(request, review_id):
 
 def Hotel_list(request):
     """Use this function to display the hotel list."""
-    Hotel_list = Hotel.objects.order_by('-name')
-    context = {'wine_list': Hotel_list}
+    hotel_list = Hotel.objects.order_by('-name')
+    context = {'hotel_list': hotel_list}
     return render(request, 'reviews/Hotel_list.html', context)
+
+
+def Hotel_detail(request, hotel_id):
+    """Use this function to display the detail for each Hotel."""
+    hotel = get_object_or_404(Hotel, pk=hotel_id)
+    return render(request, 'reviews/Hotel_detail.html', {'hotel': hotel})
